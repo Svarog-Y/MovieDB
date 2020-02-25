18af1e12c96ecd2e248788d6865a474f70ab3d03
Stefan Mihajlović <stefanmihajlovic012@gmail.com>
2020-02-25 19:03:25 +0100

    Add select directories module

---
+++ select_dir.py
@@ -0,0 +1,13 @@
+### Prompt user to select directories to go through looking for movie-type files.
+
+from tkinter import Tk
+from tkinter.filedialog import askdirectory
+
+Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+filename = askdirectory() # show an "Open" dialog box and return the path to the selected file
+
+with open("selected_directories.txt", 'a+') as file:
+    content = file.readlines()
+    if not filename in content:
+        file.seek(0)
+        file.write(f"{filename}\n")
